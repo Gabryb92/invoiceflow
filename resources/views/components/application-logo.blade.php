8f27da71871fe2825c9c1fc633d230cf2e72a128
Gabriele Bonazza <gabriele.bonazza92@gmail.com>
2025-07-30 17:31:41 +0200

    aggiunta immagine logo

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,1 +1,2 @@
-<img src="{{ asset('/img/icona.svg') }}" {{ $attributes->merge(['class' => 'block mx-auto']) }} alt="Logo Web Developer">
+{{-- <img src="{{ asset('/img/icona.svg') }}" {{ $attributes->merge(['class' => 'block mx-auto']) }} alt="Logo Web Developer"> --}}
+<img src="{{ asset('/img/invoiceflow400x400.png') }}" {{ $attributes->merge(['class' => 'block mx-auto']) }} alt="Logo Web Developer">
