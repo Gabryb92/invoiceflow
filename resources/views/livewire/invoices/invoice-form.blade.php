218d995190ff610283190954189a8dc67ed34cf0
Gabriele Bonazza <gabriele.bonazza92@gmail.com>
2025-09-18 16:13:19 +0200

    prova fix note preventivo/fattura

--- resources/views/livewire/invoices/invoice-form.blade.php
+++ resources/views/livewire/invoices/invoice-form.blade.php
@@ -83,7 +83,7 @@
 
                             <div class="sm:col-span-4">
                                 <label for="notes" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{{__('Notes')}}</label>
-                                <textarea id="notes" wire:model="notes" rows="8" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"></textarea>
+                                <textarea id="notes" wire:model="notes" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"></textarea>
                                 @error('notes') <span class="text-red-500 text-sm">{{ $message }}</span> @enderror
                             </div>
 
